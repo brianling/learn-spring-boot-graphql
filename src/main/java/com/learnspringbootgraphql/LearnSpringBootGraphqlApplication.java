3f8827521911413436d69d5517e32536cb7b8b03
BrianLing <brianling@tutorabc.com>
2022-01-03 23:56:03 +0800

    init

---
+++ src/main/java/com/learnspringbootgraphql/LearnSpringBootGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.learnspringbootgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBootGraphqlApplication{
+    
+    public static void main(String[] args){
+        SpringApplication.run(LearnSpringBootGraphqlApplication.class, args);
+    }
+    
+}
